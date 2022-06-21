b590f1446bbc75c81ffc91e1af9877f2d953ec9a
Joey Young <jqyoungkm@gmail.com>
2022-06-21 16:03:17 -0500

    Add resize and reset grid functions

--- js/script.js
+++ js/script.js
@@ -2,36 +2,76 @@ const totalSize = 960;
 let divNum = 16;
 let divWidth = `${totalSize/divNum}px`;
 
-let countRow = 1;
-let countColumn = 2;
-let sentinal= true;
-
-
+const button = document.querySelector('.button');
 const container = document.querySelector('.container');
+const cleanButton=document.querySelector('.clean');
 
-for (let i = 0; i < divNum; i++) {
-    for (let j = 0; j < divNum; j++) {
-        const div = document.createElement('div');
-        div.style.width = divWidth;
-        div.style.height = divWidth;
-        div.classList.add('divStyle');
+createGrid(divNum);
+button.addEventListener('click', resetGrid);
 
-        if (countRow <= divNum) {
-            div.textContent = countRow;
-            countRow++;
-        } else if (sentinal == true) {
-            div.textContent = countColumn;
-            countColumn++;
+//Cleans the canvas when the button is clicked
+cleanButton.addEventListener('click',()=>{
+    const divs=document.querySelectorAll('.divStyle');
+    divs.forEach(div=>{
+        div.style.backgroundColor='gray';})
+})
+
+function createGrid(divNum) {
+    let countRow = 1;
+    let countColumn = 2;
+    let sentinal = true;
+
+    divWidth = `${totalSize/divNum}px`;
+
+    for (let i = 0; i < divNum; i++) {
+        for (let j = 0; j < divNum; j++) {
+            const div = document.createElement('div');
+            div.style.width = divWidth;
+            div.style.height = divWidth;
+            div.classList.add('divStyle');
+
+            if (countRow <= divNum) {
+                div.textContent = countRow;
+                countRow++;
+            } else if (sentinal == true) {
+                div.textContent = countColumn;
+                countColumn++;
+            }
+            sentinal = false;
+
+            container.appendChild(div);
+
+            //add event listener for each div
+            div.addEventListener('mouseenter', e => {
+                div.style.backgroundColor = 'black';
+            })
+
         }
-        sentinal=false;
-
-        container.appendChild(div);
-
-        //add event listener for each div
-        div.addEventListener('mouseenter', e=>{
-            div.style.backgroundColor='black';
-        })
-
+        sentinal = true;
     }
-    sentinal = true;
+}
+
+function resetGrid() {
+    let numGrid = 0;
+    numGrid = userInput();
+
+    if (numGrid != null) {
+        while (container.firstChild) {
+            container.removeChild(container.lastChild);
+        }
+
+        createGrid(numGrid);
+    }
+}
+
+function userInput() {
+    let userInput;
+    userInput = +(prompt("Enter number of squares per side for the new grid"));
+
+    //input validation
+    while (userInput > 100 || isNaN(userInput)) {
+        userInput = +(prompt("Please enter a number less or equal to 100"))
+    }
+
+    return userInput;
 }
