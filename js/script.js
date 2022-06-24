732bced6c89dbbe005e5dad574e0bacc1028fc2e
Joey Young <jqyoungkm@gmail.com>
2022-06-24 16:38:36 -0500

    Fix the RGB shading issue and clean up as well as comment  the code

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const totalSize = 960;
 let divNum = 16;
-let divWidth = `${totalSize/divNum}px`;
+let divWidth = `${totalSize/divNum-1}px`;
+//variable used to track the status of rgb toggle 
 let rgbClicked = false;
 
 const button = document.querySelector('.button');
@@ -8,27 +9,34 @@ const container = document.querySelector('.container');
 const cleanButton = document.querySelector('.clean');
 const rgbButton = document.querySelector('.rgb');
 
+//Creates the initial grid with the default values
 createGrid(divNum);
+
+//Recreate the grid with user defined values
 button.addEventListener('click', resetGrid);
 
 //Cleans the canvas when the button is clicked
 cleanButton.addEventListener('click', () => {
     const divs = document.querySelectorAll('.divStyle');
     divs.forEach(div => {
-        div.style.backgroundColor = 'gray';
+        div.style.backgroundColor = 'lightgray';
         div.classList.remove('rgbDiv');
         div.style.filter = 'brightness(100%)';
     })
 })
 
-rgbButton.addEventListener('click', generateRGB);
+//Toggle between RGB and black mode when clicked
+rgbButton.addEventListener('click', toggleRGB);
 
+//Creates the grid with a number of square divs on each side of the grid
 function createGrid(divNum) {
+    //variables that is used to count row and column
     let countRow = 1;
     let countColumn = 2;
+    //variable that used to indicate when a new row starts
     let sentinal = true;
-
-    divWidth = `${totalSize/divNum}px`;
+    //calculates the size of each div on the grid by using the total canvas size divided by number of divs and then minus the margin size on each div
+    divWidth = `${totalSize/divNum-1}px`;
 
     for (let i = 0; i < divNum; i++) {
         for (let j = 0; j < divNum; j++) {
@@ -38,6 +46,7 @@ function createGrid(divNum) {
             div.style.filter = 'brightness(100%)'
             div.classList.add('divStyle');
 
+            //if-else that is used to count rows and columns of the grid created
             if (countRow <= divNum) {
                 div.textContent = countRow;
                 countRow++;
@@ -49,7 +58,7 @@ function createGrid(divNum) {
 
             container.appendChild(div);
 
-            //add event listener for each div
+            //add event listener for each div depending on the toggle flag, the div will be black or RGB
             if (rgbClicked == false) {
                 div.addEventListener('mousedown', changeColorBlack)
                 div.addEventListener('mouseenter', changeColorBlack)
@@ -63,11 +72,12 @@ function createGrid(divNum) {
     }
 }
 
+//Resets the grid by first delete the current grid and then use the user input value to recreate a new grid
 function resetGrid() {
     let numGrid = 0;
     numGrid = userInput();
-    console.log(numGrid);
 
+    //deletes the current grid if user input isn't 0 and then create a new grid with the new values
     if (numGrid != 0) {
         while (container.firstChild) {
             container.removeChild(container.lastChild);
@@ -77,6 +87,7 @@ function resetGrid() {
     }
 }
 
+//Asks for user input to determine how many squares per side on the grid
 function userInput() {
     let userInput;
     userInput = +(prompt("Enter number of squares per side for the new grid"));
@@ -89,7 +100,8 @@ function userInput() {
     return userInput;
 }
 
-function generateRGB() {
+//Toggles individual grid color to RGB from black or vise versa when the button is clicked
+function toggleRGB() {
     const divs = document.querySelectorAll('.divStyle');
     if (!rgbClicked) {
         let rgbColor = '';
@@ -101,7 +113,6 @@ function generateRGB() {
 
 
         divs.forEach(div => {
-
             div.removeEventListener('mousedown', changeColorBlack);
             div.removeEventListener('mouseenter', changeColorBlack);
 
@@ -120,10 +131,9 @@ function generateRGB() {
         })
         rgbClicked = false;
     }
-    console.log(rgbClicked);
 }
 
-
+//Changes div color to black
 function changeColorBlack(event) {
     const div = event.target;
     if (event.type == 'mousedown') {
@@ -136,14 +146,20 @@ function changeColorBlack(event) {
     }
 }
 
+//Changes div color to RGB as well as add 10% black to it with each pass
 function changeRgbColor(event) {
     const div = event.target;
     let rgbColor;
+    //variable used to calculate the brightness level on each div
     let currentBright;
     let filterValue;
+    
+    //determines if the event type is either mousedown or mouseenter
     if (event.type == 'mousedown') {
         event.preventDefault();
-        rgbColor = `rgb(${Math.floor(Math.random()*256)}, ${Math.floor(Math.random()*256)}, ${Math.floor(Math.random()*256)})`
+        //generates a random rgb color and assign it to the variable
+        rgbColor = `rgb(${Math.floor(Math.random()*256)}, ${Math.floor(Math.random()*256)}, ${Math.floor(Math.random()*256)})`  
+        //if the div already has rgb color then darken it by 10%, else if the div has not been passed through before then add rgb color to it
         if (event.target.classList.contains('rgbDiv')) {
             if (div.style.filter == 'brightness(100%)') {
                 filterValue = div.style.filter;
@@ -177,5 +193,5 @@ function changeRgbColor(event) {
             }
 
         }
-    }
-}
+    }//end of if else-if
+}//end of function
